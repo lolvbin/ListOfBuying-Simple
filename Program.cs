eec886e4c072a58f2e1e89fb189ddf9dd58fb9b8
Lovbin <vitorlobofribeiro@gmail.com>
2026-03-04 14:33:37 -0300

    Subindo atividade extra realizada com junção dos conceitos das outra realizadas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<string> Compras = new List<string>();
+string item = "";
+
+Console.WriteLine("Lista de Compras Simples");
+
+System.Console.WriteLine("Informe o item a ser adicionado ao carrinho: ");
+item = Console.ReadLine();
+
+while (item != "Sair") {
+    System.Console.WriteLine("Proximo item: ");
+    item = Console.ReadLine();
+    if (item != "Sair")
+    {
+        Compras.Add(item);
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine("- Esta é sua lista de compras -");
+foreach(string thing in Compras)
+{
+    System.Console.WriteLine(thing);
+}
--- obj/Debug/net9.0/ListBuying.AssemblyInfo.cs
+++ obj/Debug/net9.0/ListBuying.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListBuying")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079bf00f95a7bbd04895334de9f17fc4b780a45c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6c819adf98c779d61c26070054c476265a41b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListBuying")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListBuying")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
